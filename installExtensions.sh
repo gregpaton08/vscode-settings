a43fe7f97813c14b9b90acac7eb00b9bfd40cd58
Gregory Paton <gregpaton08@gmail.com>
2019-05-14 20:56:28 -0400

    Adds option to script to update extensions.txt with currently installed extensions

--- installExtensions.sh
+++ installExtensions.sh
@@ -1,9 +1,31 @@
+operation=install
+while getopts ":u" opt; do
+    case ${opt} in
+        u )
+            operation=update
+            ;;
+        /? )
+            echo "Invalid Option: -$OPTARG" 1>&2
+            print_help
+            exit 1
+            ;;
+    esac
+done
+
 if [ $(command -v code) ]; then
     code="code"
 else
     code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
 fi
 
-while read line; do
-    "$code" --install-extension "$line"
-done <extensions.txt
+install() {
+    while read line; do
+        "$code" --install-extension "$line"
+    done <extensions.txt
+}
+
+update() {
+    "$code" --list-extensions > extensions.txt
+}
+
+eval $operation
