5a6f0c4f81083c615c09ce32d654c6c920375e9a
Greg Paton <gregpaton08@gmail.com>
2019-05-14 20:50:28 -0400

    Update installExtensions.sh

--- installExtensions.sh
+++ installExtensions.sh
@@ -1,5 +1,9 @@
-code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+if [ $(command -v code) ]; then
+    code="code"
+else
+    code="/Applications/Visual Studio Code.app/Contents/Resources/app/bin/code"
+fi
+
 while read line; do
     "$code" --install-extension "$line"
 done <extensions.txt
-
